718cd85cde769ba46cfb6c72f6d0db6f5c247858
Angel Murcia Diaz <angel.ns.333@gmail.com>
2019-11-18 21:26:33 +0100

    Añadidas nuevas funciones

--- src/Portatiles.py
+++ src/Portatiles.py
@@ -2,6 +2,28 @@ class Portatiles:
     def __init__ (self):
         self.lista_portatiles = []
 
+    def numeroPortatilesEnVenta(self):
+        return len(self.lista_portatiles)
+
     def agregarPortatil(self, marca, modelo, DNIvendedor, precio):
         portatil = {marca=marca;modelo=modelo;DNIvendedor=DNIvendedor;precio=precio}
         self.lista_portatiles.append(portatil)
+        id = numeroPortatilesEnVenta(self)
+        return id
+
+    def eliminarPortatilPorId(self, id):
+        if id >= numeroPortatilesEnVenta():
+            del(self.lista_portatiles[id])
+            return true
+        else:
+            return false
+
+    def seleccionarPortatil(self, id):
+        if id >= numeroPortatilesEnVenta():
+            return self.lista_portatiles[id]
+        else:
+            return false
+
+    def listar(self):
+        for i in self.lista_portatiles:
+            print(i)
