73edf2dd91eaad1f8d0f1e226ccf6ef86661bbe7
Angel Murcia Diaz <angel.ns.333@gmail.com>
2019-11-18 22:22:31 +0100

    Añadidas funciones

--- src/Portatiles.py
+++ src/Portatiles.py
@@ -6,23 +6,32 @@ class Portatiles:
         return len(self.lista_portatiles)
 
     def agregarPortatil(self, marca, modelo, DNIvendedor, precio):
-        portatil = {"marca":marca,"modelo":modelo,"DNIvendedor":DNIvendedor,"precio":precio}
+        id_venta = self.numeroPortatilesEnVenta()+1
+        portatil = {"IDventa":id_venta, "marca":marca,"modelo":modelo,"DNIvendedor":DNIvendedor,"precio":precio}
         self.lista_portatiles.append(portatil)
-        id = numeroPortatilesEnVenta(self)
-        return id
+        return id_venta
 
-    def eliminarPortatilPorId(self, id):
-        if id >= numeroPortatilesEnVenta():
-            del(self.lista_portatiles[id])
-            return true
-        else:
-            return false
+    def indiceEnListaIDventa(self, id_venta):
+        indice = False
+        for i, portatil in enumerate(self.lista_portatiles):
+            if portatil.get("IDventa") == id_venta:
+                return i
+        return indice
 
-    def seleccionarPortatil(self, id):
-        if id >= numeroPortatilesEnVenta():
-            return self.lista_portatiles[id]
+    def eliminarPortatilPorIdVenta(self, id_venta):
+        indice = self.indiceEnListaIDventa(id_venta)
+        if indice != False:
+            del(self.lista_portatiles[indice])
+            return True
         else:
-            return false
+            return False
+
+    def seleccionarPortatil(self, id_venta):
+        indice = self.indiceEnListaIDventa(id_venta)
+        if indice != False:
+            return self.lista_portatiles[indice]
+        else:
+            return False
 
     def listar(self):
         for i in self.lista_portatiles:
