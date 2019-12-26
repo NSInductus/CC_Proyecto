c16a3dafdad91aca9bf622868e74222137845948
Angel Murcia Diaz <angel.ns.333@gmail.com>
2019-12-26 20:18:24 +0100

    Acabados test para la API REST de gestión de portatiles closes #27

--- tests/test_Portatiles_rest.py
+++ tests/test_Portatiles_rest.py
@@ -10,6 +10,12 @@ def test_prueba():
     respuesta = app.get('/')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
@@ -22,6 +28,12 @@ def test_rest_numero_portatiles():
     respuesta = app.get('/numeroPortatilesEnVenta')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
@@ -30,34 +42,64 @@ def test_rest_seleccionar_portatil():
     respuesta = app.get('/seleccionarPortatil/1')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
 def test_rest_agregar_portatil_version_corta():
-    respuesta = app.get('/agregarPortatil/msi/gl62/333X/600')
+    respuesta = app.post('/agregarPortatil/msi/gl62/333X/600')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
 def test_rest_agregar_portatil_version_larga():
-    respuesta = app.get('/agregarPortatil/msi/gl62/333X/600/Muy%20bueno/15/i7/GB%20DDR4/1gb/GTX/2H/Linux')
+    respuesta = app.post('/agregarPortatil/msi/gl62/333X/600/Muy%20bueno/15/i7/GB%20DDR4/1gb/GTX/2H/Linux')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
 def test_rest_modificar_portatil_solo_precio():
-    respuesta = app.get('/modificarPortatil/1/800000')
+    respuesta = app.put('/modificarPortatil/1/800000')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
 def test_rest_eliminar_portatil():
-    respuesta = app.get('/eliminarPortatilPorIdVenta/1')
+    respuesta = app.delete('/eliminarPortatilPorIdVenta/1')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
@@ -65,6 +107,12 @@ def test_rest_buscar_portatil_dnivendedor():
     respuesta = app.get('/verPortatilesEnVentaDeUsuario/333X')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
@@ -72,6 +120,12 @@ def test_rest_buscar_portatil_precio():
     respuesta = app.get('/buscarPortatilPorPrecio/500/1000')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
 
@@ -79,13 +133,24 @@ def test_rest_busar_portatil_modelo_marca():
     respuesta = app.get('/buscarPortatilPorModeloMarca/gl62/msi')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
-
 
 def test_rest_comparar_portatiles():
     respuesta = app.get('/compararPotatiles/gl62/msi')
     if (respuesta.status_code == 200):
         assert (respuesta.status_code == 200 and respuesta.headers["Content-Type"] == "application/json")
+    elif (respuesta.status_code == 413):
+        assert (respuesta.status_code == 413)
+    elif (respuesta.status_code == 414):
+        assert (respuesta.status_code == 414)
+    elif (respuesta.status_code == 429):
+        assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
