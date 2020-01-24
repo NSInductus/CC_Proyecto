f90b53938d7d8e9b3597d1e681c67e869a7a8de5
Angel Murcia Diaz <angel.ns.333@gmail.com>
2020-01-24 17:21:14 +0100

    Modificación de Portatiles para adaptar la clase al data_manager

--- src/Portatiles.py
+++ src/Portatiles.py
@@ -1,48 +1,30 @@
 from reportlab.pdfgen import canvas
 
+#Clase Portatiles: contenedora de la lógica de negocio del microservicio Portatiles
 class Portatiles:
-    def __init__ (self):
-        self.lista_portatiles = []
-        self.indice = 1
+    def __init__ (self, data_manager):
+        #Se inicializa con un data_manager
+        self.data_manager = data_manager
 
     def numeroPortatilesEnVenta(self):
-        return len(self.lista_portatiles)
+        return self.data_manager.numero_elementos()
 
     def agregarPortatil(self, marca, modelo, DNIvendedor, precio, comentario="", pantalla="", procesador="", RAM="", almacenamiento="", grafica="", bateria="", SO=""):
-        id_venta = self.indice
-        self.indice = self.indice + 1
-        portatil = {"IDventa":id_venta, "marca":marca,"modelo":modelo, "pantalla":pantalla, "procesador":procesador, "RAM":RAM, "almacenamiento":almacenamiento, "grafica":grafica, "bateria":bateria, "SO":SO, "DNIvendedor":DNIvendedor, "comentario":comentario, "precio":precio}
-        self.lista_portatiles.append(portatil)
-        return id_venta
+        portatil = {"marca":marca,"modelo":modelo, "pantalla":pantalla, "procesador":procesador, "RAM":RAM, "almacenamiento":almacenamiento, "grafica":grafica, "bateria":bateria, "SO":SO, "DNIvendedor":DNIvendedor, "comentario":comentario, "precio":precio}
+        _id = self.data_manager.insertar_elemento(portatil)
+        return _id
 
-    def existeEnListaIDventa(self, id_venta):
-        for i, portatil in enumerate(self.lista_portatiles):
-            if portatil.get("IDventa") == id_venta:
-                return True
-        return False
-
-    def indiceEnListaIDventa(self, id_venta):
-        existe = self.existeEnListaIDventa(id_venta)
+    def eliminarPortatilPorIdVenta(self, _id):
+        existe = self.data_manager.obtener_elemento("_id", _id)
         if existe != False:
-            for i, portatil in enumerate(self.lista_portatiles):
-                if portatil.get("IDventa") == id_venta:
-                    return i
-        return False
-
-    def eliminarPortatilPorIdVenta(self, id_venta):
-        existe = self.existeEnListaIDventa(id_venta)
-        if existe != False:
-            indice = self.indiceEnListaIDventa(id_venta)
-            del(self.lista_portatiles[indice])
+            self.data_manager.borrar_elemento(_id)
             return True
         else:
             return False
 
     def modificarPortatil(self, id_venta, precio="", modelo="", marca="",comentario="",  pantalla="", procesador="", RAM="", almacenamiento="", grafica="", bateria="", SO=""):
-        existe = self.existeEnListaIDventa(id_venta)
+        existe = self.data_manager.obtener_elemento("_id", _id)
         if existe != False:
-            indice = self.indiceEnListaIDventa(id_venta)
-            portatil = self.seleccionarPortatil(id_venta)
             if comentario == "":
                 comentario = portatil.get("comentario")
             if precio == "":
@@ -65,30 +47,25 @@ class Portatiles:
                 marca = portatil.get("marca")
             if modelo == "":
                 modelo = portatil.get("modelo")
-            dni = portatil.get("DNIvendedor")
-            self.eliminarPortatilPorIdVenta(id_venta)
-            portatil_nuevo = {"IDventa":id_venta, "marca":marca,"modelo":modelo, "pantalla":pantalla, "procesador":procesador, "RAM":RAM, "almacenamiento":almacenamiento, "grafica":grafica, "bateria":bateria, "SO":SO, "DNIvendedor":dni, "comentario":comentario, "precio":precio}
-            self.lista_portatiles.append(portatil_nuevo)
+            portatil_nuevo = {"marca":marca,"modelo":modelo, "pantalla":pantalla, "procesador":procesador, "RAM":RAM, "almacenamiento":almacenamiento, "grafica":grafica, "bateria":bateria, "SO":SO, "DNIvendedor":dni, "comentario":comentario, "precio":precio}
+            self.data_manager.actualizar_elemento(_id, portatil_nuevo)
             return True
         else:
             return False
 
-    def seleccionarPortatil(self, id_venta):
-        existe = self.existeEnListaIDventa(id_venta)
-        if existe != False:
-            indice = self.indiceEnListaIDventa(id_venta)
-            return self.lista_portatiles[indice]
-        else:
-            return False
+    def seleccionarPortatil(self, _id):
+        return self.data_manager.obtener_elemento("_id", _id)
 
     def portatiles_en_venta_de_usuario(self, DNIusuario):
+        lista_portatiles = self.data_manager.obtener_todos_elementos()
         portatiles_usuario = []
-        for i, portatil in enumerate(self.lista_portatiles):
+        for i, portatil in enumerate(lista_portatiles):
             if portatil.get("DNIvendedor") == DNIusuario:
                 portatiles_usuario.append(portatil)
         return portatiles_usuario
 
     def buscar_portatil_por_precio(self, limite_inferior, limite_superior):
+        lista_portatiles = self.data_manager.obtener_todos_elementos()
         portatiles_busqueda = []
         for i, portatil in enumerate(self.lista_portatiles):
             if (portatil.get("precio") <= limite_superior) and (portatil.get("precio") >= limite_inferior):
@@ -96,6 +73,7 @@ class Portatiles:
         return portatiles_busqueda
 
     def buscar_portatil_por_modelo_marca(self, modelo, marca):
+        lista_portatiles = self.data_manager.obtener_todos_elementos()
         portatiles_busqueda = []
         for i, portatil in enumerate(self.lista_portatiles):
             if (portatil.get("modelo") == modelo) and (portatil.get("marca") == marca):
@@ -104,6 +82,7 @@ class Portatiles:
 
     #Te compara portatiles de la misma marca y modelo y te los ordena por precio
     def comparar_portatiles(self, modelo, marca):
+        lista_portatiles = self.data_manager.obtener_todos_elementos()
         portatiles_iguales = []
         for i, portatil in enumerate(self.lista_portatiles):
             if (portatil.get("modelo") == modelo) and (portatil.get("marca") == marca):
