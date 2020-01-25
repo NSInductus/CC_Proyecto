195ff66e060fcd5ae92286822e35d419633d33c7
Angel Murcia Diaz <angel.ns.333@gmail.com>
2020-01-25 01:16:52 +0100

    Creacion de nueva funcion de test y eliminacion de comentario que no son útiles

--- tests/test_Transaciones.py
+++ tests/test_Transaciones.py
@@ -16,8 +16,6 @@ from MongoDM import MongoDM
 data_manager = MongoDM(os.environ['URI_ENVIRON'],'Nueva','COTransaciones')
 nueva_lista = Transaciones(data_manager)
 
-
-
 def funcion_inicio():
     data_manager = MongoDM(os.environ['URI_ENVIRON'],'Nueva','COTransaciones')
     nueva_lista = Transaciones(data_manager)
@@ -34,17 +32,14 @@ def cliente_test():
     yield cliente_test
     funcion_fin()
 
-
 def test_lista_transaciones_vacia(cliente_test):
     #nueva_lista.limpiarLista()
     assert nueva_lista.numeroTransaciones() == 0
     assert nueva_lista.seleccionarTransacion("1") == False
 
 def test_agregar_y_seleccionar_nueva_transacion(cliente_test):
-    #nueva_lista.limpiarLista()
     indice = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
     assert nueva_lista.numeroTransaciones() == 1
-    #portatil = {"_id":bson.ObjectId(indice), "marca":"MSI", "modelo":"GL62M", "DNIvendedor":"333X", "precio":2000, "pantalla":"", "procesador":"", "RAM":"", "almacenamiento":"", "grafica":"", "bateria":"", "SO":"",  "comentario":"", "vendido":0}
     transacion = {"_id":bson.ObjectId(indice), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":""}
     assert nueva_lista.seleccionarTransacion(indice) == transacion
 
@@ -60,12 +55,26 @@ def test_agregar_y_seleccionar_nueva_transacion(cliente_test):
 
 
 def test_agregar_y_seleccionar_nuevo_transacion_con_comentario(cliente_test):
-    #nueva_lista.limpiarLista()
     indice = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1, "Soy un comentario")
     assert nueva_lista.numeroTransaciones() == 1
     #portatil = {"_id":bson.ObjectId(indice), "marca":"MSI", "modelo":"GL62M", "DNIvendedor":"333X", "precio":2000, "pantalla":"", "procesador":"", "RAM":"", "almacenamiento":"", "grafica":"", "bateria":"", "SO":"",  "comentario":"", "vendido":0}
     transacion = {"_id":bson.ObjectId(indice), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":"Soy un comentario"}
     assert nueva_lista.seleccionarTransacion(indice) == transacion
+
+def test_agregar_varias_transaciones(cliente_test):
+    indice1 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
+    indice2 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
+    indice3 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
+    indice4 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
+    assert nueva_lista.numeroTransaciones() == 4
+    transacion1 = {"_id":bson.ObjectId(indice1), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":""}
+    transacion2 = {"_id":bson.ObjectId(indice2), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":""}
+    transacion3 = {"_id":bson.ObjectId(indice3), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":""}
+    transacion4 = {"_id":bson.ObjectId(indice4), "id_portatil": "e23rewr2wqe43242t", "DNIvendedor": "333X", "tipo":1, "comentario":""}
+    assert nueva_lista.seleccionarTransacion(indice1) == transacion1
+    assert nueva_lista.seleccionarTransacion(indice2) == transacion2
+    assert nueva_lista.seleccionarTransacion(indice3) == transacion3
+    assert nueva_lista.seleccionarTransacion(indice4) == transacion4
 
 def test_eliminar_portatil(cliente_test):
     #nueva_lista.limpiarLista()
@@ -79,8 +88,6 @@ def test_eliminar_portatil(cliente_test):
     indice2 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1, "Soy un comentario")
     assert nueva_lista.eliminarTransacion(indice) == True
     assert nueva_lista.numeroTransaciones() == 1
-
-
 
 def test_estadisticas(cliente_test):
     nueva_lista.limpiarLista()
@@ -99,8 +106,6 @@ def test_estadisticas(cliente_test):
     mis_transaciones = nueva_lista.verEstadisticas("333X")
     assert len(mis_transaciones) == 1
     assert mis_transaciones[0] == transacion3
-
-
 
 def test_imprimir_comparacion(cliente_test):
     nueva_lista.limpiarLista()
