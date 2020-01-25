ba2d4469a6e6b618326ff95680bf1efab5eeb9f0
Angel Murcia Diaz <angel.ns.333@gmail.com>
2020-01-25 21:16:25 +0100

    Documentada funcion de test de la impresion closes #20

--- tests/test_Transaciones.py
+++ tests/test_Transaciones.py
@@ -7,11 +7,7 @@ sys.path.append('src')
 import bson
 
 from Transaciones import Transaciones
-#from Portatiles import Portatiles
 from MongoDM import MongoDM
-
-#data_manager = MongoDM(os.environ['URI_ENVIRON'],os.environ['BD_ENVIRON'],os.environ['CO_ENVIRON'])
-#nueva_lista_p = Portatiles(data_manager)
 
 data_manager = MongoDM(os.environ['URI_BD_T'], os.environ['BD_T'], os.environ['CO_T'])
 nueva_lista = Transaciones(data_manager)
@@ -21,19 +17,12 @@ def funcion_inicio():
     nueva_lista = Transaciones(data_manager)
     nueva_lista.limpiarLista()
 
-def funcion_fin():
-    data_manager = MongoDM(os.environ['URI_BD_T'], os.environ['BD_T'], os.environ['CO_T'])
-    nueva_lista = Transaciones(data_manager)
-    nueva_lista.limpiarLista()
-
 @pytest.fixture
 def cliente_test():
     funcion_inicio()
     yield cliente_test
-    funcion_fin()
 
 def test_lista_transaciones_vacia(cliente_test):
-    #nueva_lista.limpiarLista()
     assert nueva_lista.numeroTransaciones() == 0
     assert nueva_lista.seleccionarTransacion("1") == False
 
@@ -42,17 +31,6 @@ def test_agregar_y_seleccionar_nueva_transacion(cliente_test):
     assert nueva_lista.numeroTransaciones() == 1
     transacion = {"_id":bson.ObjectId(indice), "id_portatil": "e23rewr2wqe43242t", "DNIusuario": "333X", "tipo":1, "comentario":""}
     assert nueva_lista.seleccionarTransacion(indice) == transacion
-
-#def test_agregar_nuevo_portatil():
-#    nueva_lista.limpiarLista()
-#    nueva_lista_p.limpiarLista()
-#    i = nueva_lista_p.agregarPortatil("MSI","GL62M","333X",2000)
-#    indice = nueva_lista.agregarTransacion(i, "333X", 1)
-#    assert nueva_lista.numeroTransaciones() == 1
-    #portatil = {"_id":bson.ObjectId(indice), "marca":"MSI", "modelo":"GL62M", "DNIusuario":"333X", "precio":2000, "pantalla":"", "procesador":"", "RAM":"", "almacenamiento":"", "grafica":"", "bateria":"", "SO":"",  "comentario":"", "vendido":0}
-#    transacion = {"_id":bson.ObjectId(indice), "id_portatil": i, "DNIusuario": "333X", "tipo":1, "comentario":""}
-#    assert nueva_lista.seleccionarTransacion(indice) == transacion
-
 
 def test_agregar_y_seleccionar_nuevo_transacion_con_comentario(cliente_test):
     indice = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1, "Soy un comentario")
@@ -107,6 +85,7 @@ def test_estadisticas(cliente_test):
     assert len(mis_transaciones) == 1
     assert mis_transaciones[0] == transacion3
 
+#Realmente lo que es el resultado del PFD no se puede testear
 def test_imprimir_comparacion(cliente_test):
     nueva_lista.limpiarLista()
     indice1 = nueva_lista.agregarTransacion("e23rewr2wqe43242t", "333X", 1)
