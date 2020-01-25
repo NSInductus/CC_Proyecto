cd0de4d551d71ffc2f7c1cdcb02b7232148409f9
Angel Murcia Diaz <angel.ns.333@gmail.com>
2020-01-25 21:18:34 +0100

    Eliminas lineas inecesaria y añadido comentario de estadisticas closes #19

--- tests/test_Transaciones_rest.py
+++ tests/test_Transaciones_rest.py
@@ -14,30 +14,17 @@ from Portatiles import Portatiles
 from MongoDM import MongoDM
 
 def funcion_inicio():
-    #data_manager = MongoDM(os.environ['URI_ENVIRON'],os.environ['BD_ENVIRON'],os.environ['CO_ENVIRON'])
-    #portatiles = Portatiles(data_manager)
-    #indice1 = portatiles.agregarPortatil("msi","gl62","333X",600)
+
     data_manager = MongoDM(os.environ['URI_BD_T'],os.environ['BD_T'],os.environ['CO_T'])
     transaciones = Transaciones(data_manager)
     transaciones.limpiarLista()
-    #return indice1
-
-def funcion_fin():
-    data_manager = MongoDM(os.environ['URI_BD_P'],os.environ['BD_P'],os.environ['CO_P'])
-    portatiles = Portatiles(data_manager)
-    portatiles.limpiarLista()
-    #data_manager = MongoDM(os.environ['URI_ENVIRON'],os.environ['BDT_ENVIRON'],os.environ['COT_ENVIRON'])
-    #transaciones = Transaciones(data_manager)
-    #transaciones.limpiarLista()
 
 @pytest.fixture
 def cliente_test():
     funcion_inicio()
     cliente_test = Transaciones_rest.app.test_client()
     cliente_test_portatil = Portatiles_rest.app.test_client()
-    #Transaciones_rest.app.config['TESTING'] = test_prueba
     yield cliente_test, cliente_test_portatil
-    funcion_fin()
 
 
 def test_transaciones_rest_prueba(cliente_test):
@@ -58,7 +45,6 @@ def test_transaciones_rest_ruta_desconocida(cliente_test):
     respuesta = cliente_test[0].get('/transaciones/kokoko')
     if (respuesta.status_code == 204):
         assert (respuesta.status_code == 204)
-
 
 def test_transaciones_rest_prueba_vender_portatil(cliente_test):
     #Seleccionar portatil
@@ -98,7 +84,6 @@ def test_transaciones_rest_prueba_vender_portatil(cliente_test):
     cadena = '/transaciones/venderPortatil/' + indice + '/' + DNIcomprador
     respuesta = cliente_test[0].post(cadena)
 
-
 def test_transaciones_rest_prueba_devolver_portatil(cliente_test):
     #Seleccionar portatil
     cadena = '/portatiles/agregarPortatil/msi/gl62/333X/600'
@@ -137,8 +122,7 @@ def test_transaciones_rest_prueba_devolver_portatil(cliente_test):
     cadena = '/transaciones/devolverPortatil/' + indice + '/' + DNIcomprador
     respuesta = cliente_test[0].post(cadena)
 
-
-
+#Funciones de ver estadisticas del API
 def test_transaciones_rest_ver_estadisticas(cliente_test):
     lista = []
     respuesta = cliente_test[0].get('/transaciones/verEstadisticas/666X')
@@ -153,7 +137,6 @@ def test_transaciones_rest_ver_estadisticas(cliente_test):
         assert (respuesta.status_code == 429)
     else:
         assert (respuesta.status_code == 412)
-
 
 def test_transaciones_rest_ver_estadisticas_tipo(cliente_test):
     lista = []
